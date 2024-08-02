05167bfd84774ce00bc0991df772f0bf7f4434ad
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-08-01 23:07:39 -0500

    Make RiotAuthentication public

--- BlossomiShymae.GrrrLCU/RiotAuthentication.cs
+++ BlossomiShymae.GrrrLCU/RiotAuthentication.cs
@@ -3,12 +3,29 @@ using System.Text;
 
 namespace BlossomiShymae.GrrrLCU
 {
-    internal class RiotAuthentication(string RemotingAuthToken)
+    /// <summary>
+    /// Repesents authentication for the League Client.
+    /// </summary>
+    /// <param name="RemotingAuthToken"></param>
+    public class RiotAuthentication(string RemotingAuthToken)
     {
+        /// <summary>
+        /// Password component of the authentication.
+        /// </summary>
         public string Password { get; } = RemotingAuthToken;
+        /// <summary>
+        /// Authentication value before Base64 conversion.
+        /// </summary>
         public string RawValue { get => $"riot:{Password}"; }
+        /// <summary>
+        /// Authentication value in Base64 format.
+        /// </summary>
         public string Value { get => Convert.ToBase64String(Encoding.UTF8.GetBytes(RawValue)); }
 
+        /// <summary>
+        /// Get an AuthenticationHeaderValue instance.
+        /// </summary>
+        /// <returns></returns>
         public AuthenticationHeaderValue ToAuthenticationHeaderValue()
         {
             return new AuthenticationHeaderValue("Basic", Value);
