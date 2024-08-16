3c08fff5382ee2918a18f4ccae4b213e165f6a5a
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-08-16 06:20:11 -0500

    Add better exception information when failing to retrieve command line

--- BlossomiShymae.GrrrLCU/ProcessInfo.cs
+++ BlossomiShymae.GrrrLCU/ProcessInfo.cs
@@ -42,7 +42,7 @@ namespace BlossomiShymae.GrrrLCU
             } 
             else
             { 
-                throw new InvalidOperationException("Failed to retrieve process command line."); 
+                throw new InvalidOperationException(ProcessCommandLine.ErrorToString(rc)); 
             }
         }
     }
