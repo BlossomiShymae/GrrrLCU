5007a4d604ba5dcfc30844c5ba99adbf5f189324
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-08-19 21:43:07 -0500

    Remove extern code, use WMI or lockfile when getting arguments

--- BlossomiShymae.GrrrLCU/EventMessage.cs
+++ BlossomiShymae.GrrrLCU/EventMessage.cs
@@ -1,5 +1,3 @@
-using System.Text.Json;
-using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 
 namespace BlossomiShymae.GrrrLCU
--- BlossomiShymae.GrrrLCU/ProcessInfo.cs
+++ BlossomiShymae.GrrrLCU/ProcessInfo.cs
@@ -21,10 +21,55 @@ namespace BlossomiShymae.GrrrLCU
 
         internal ProcessInfo(Process process)
         {
-            var rc = ProcessCommandLine.Retrieve(process, out var cl);
-            if (rc == 0)
+            if (TryGetCommandLineArguments(process, out var token, out var port, out var commandException))
             {
-                var args = ProcessCommandLine.CommandLineToArgs(cl);
+                RemotingAuthToken = token;
+                AppPort = port;
+            }
+            else if (TryGetLockfile(process, out var _token, out var _port, out var lockException))
+            {
+                RemotingAuthToken = _token;
+                AppPort = _port;
+            }
+            else
+            {
+                throw new AggregateException("Unable to obtain process information.", [commandException!, lockException!]);
+            }
+        }
+
+        private bool TryGetLockfile(Process process, out string token, out int port, out Exception? ex)
+        {
+            try
+            {
+                var path = Directory.GetParent(process.MainModule!.FileName)!.FullName;
+                var lockfilePath = Path.Join(path, "lockfile");
+
+                using var lockfileStream = File.Open(lockfilePath, FileMode.Open, FileAccess.Read, FileShare.Write);
+                using var lockfileReader = new StreamReader(lockfileStream);
+
+                var lockfile = lockfileReader.ReadToEnd();
+                var values = lockfile.Split(":");
+
+                token = values[3];
+                port = int.Parse(values[2]);
+                ex = null;
+                return true;
+            }
+            catch (Exception e)
+            {
+                token = string.Empty;
+                port = 0;
+                ex = e;
+                return false;
+            }
+        }
+
+        private bool TryGetCommandLineArguments(Process process, out string remotingAuthToken, out int appPort, out Exception? ex)
+        {
+            try
+            {
+                var args = process.GetCommandLineArguments();
+    
                 foreach (var arg in args)
                 {
                     if (arg.Length > 0 && arg.Contains('='))
@@ -32,17 +77,22 @@ namespace BlossomiShymae.GrrrLCU
                         var split = arg[2..].Split("=");
                         var key = split[0];
                         var value = split[1];
-
+    
                         _args[key] = value;
                     }
                 }
-
-                RemotingAuthToken = _args["remoting-auth-token"];
-                AppPort = int.Parse(_args["app-port"]);
-            } 
-            else
-            { 
-                throw new InvalidOperationException(ProcessCommandLine.ErrorToString(rc)); 
+    
+                remotingAuthToken = _args["remoting-auth-token"];
+                appPort = int.Parse(_args["app-port"]);
+                ex = null;
+                return true;
+            }
+            catch (Exception e)
+            {
+                remotingAuthToken = string.Empty;
+                appPort = 0;
+                ex = e;
+                return false;
             }
         }
     }
