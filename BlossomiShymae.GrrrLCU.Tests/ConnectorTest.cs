d8e8ec4036b6671f9cdebb9a84476ba66a1fbc35
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-08-21 23:55:50 -0500

    Update tests

--- BlossomiShymae.GrrrLCU.Tests/ConnectorTest.cs
+++ BlossomiShymae.GrrrLCU.Tests/ConnectorTest.cs
@@ -6,19 +6,21 @@ namespace BlossomiShymae.GrrrLCU.Tests
     public class ConnectorTest
     {
         private readonly ITestOutputHelper _output;
+        private readonly LcuHttpClient _lcuHttpClient;
 
         public ConnectorTest(ITestOutputHelper output)
         {
             _output = output;
+            _lcuHttpClient = Connector.GetLcuHttpClientInstance();
         }
 
         [Fact]
         public async Task SendAsyncTest()
         {
-            var response = await Connector.SendAsync(HttpMethod.Get, "/lol-summoner/v1/current-summoner");
+            var response = await _lcuHttpClient.SendAsync(new(HttpMethod.Get, "/lol-summoner/v1/current-summoner"));
             
             var data = await response.Content.ReadFromJsonAsync<Summoner>();
-            _output.WriteLine($"{nameof(Connector.SendAsync)}: {data?.GameName}");
+            _output.WriteLine($"{data?.GameName}");
 
             Assert.True(data != null);
         }
@@ -26,10 +28,10 @@ namespace BlossomiShymae.GrrrLCU.Tests
         [Fact]
         public async Task GetAsyncTest()
         {
-            var response = await Connector.GetAsync("/lol-summoner/v1/current-summoner");
+            var response = await _lcuHttpClient.GetAsync("/lol-summoner/v1/current-summoner");
 
             var data = await response.Content.ReadFromJsonAsync<Summoner>();
-            _output.WriteLine($"{nameof(Connector.GetAsync)}: {data?.GameName}");
+            _output.WriteLine($"{data?.GameName}");
 
             Assert.True(data != null);
         }
@@ -37,8 +39,8 @@ namespace BlossomiShymae.GrrrLCU.Tests
         [Fact]
         public async Task GetFromJsonAsyncTest()
         {
-            var data = await Connector.GetFromJsonAsync<Summoner>("/lol-summoner/v1/current-summoner");
-            _output.WriteLine($"{nameof(Connector.GetFromJsonAsync)}: {data?.GameName}");
+            var data = await _lcuHttpClient.GetFromJsonAsync<Summoner>("/lol-summoner/v1/current-summoner");
+            _output.WriteLine($"{data?.GameName}");
 
             Assert.True(data != null);
         }
